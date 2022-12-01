1d2efd4d5ea2efebb00fcba5ab0401799ce04110
jsbento <benton_jacob@yahoo.com>
2022-12-01 14:25:53 -0600

    redesign light

--- CS336Light.js
+++ CS336Light.js
@@ -1,29 +1,31 @@
-class CS336Light { // subtype of cs336 following camera
-    DEFAULT_LIGHT_PROPERTIES = new Float32Array([
-        0.2, 0.2, 0.2,
-        0.7, 0.7, 0.7,
-        0.7, 0.7, 0.7,
-    ])
+var CS336Light = function({x, y, z, lightProperties} = {x: 0.0, y: 0.0, z: 2.0, lightProperties: CS336Light.DEFAULT_LIGHT_PROPERTIES}) {
+    CS336Object.call(this);
 
-    x = 0.0;
-    y = 0.0;
-    z = 0.0;
-    w = 1.0;
-    lightProperties = null;
+    this.setPosition(x, y, z);
 
-    constructor({x, y, z, lightProperties} = {x: 0.0, y: 0.0, z: 2.0, lightProperties: this.DEFAULT_LIGHT_PROPERTIES}) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.w = 1.0;
-        this.lightProperties = lightProperties;
-    }
+    this.lightProperties = lightProperties;
+}
 
-    get position() {
-        return {
-            x: this.x,
-            y: this.y,
-            z: this.z
-        }
+CS336Light.prototype = Object.create(CS336Object.prototype);
+
+CS336Light.prototype.DEFAULT_LIGHT_PROPERTIES = new Float32Array([
+    0.2, 0.2, 0.2,
+    0.7, 0.7, 0.7,
+    0.7, 0.7, 0.7,
+]);
+
+CS336Light.prototype.getLightProperties = function() {
+    return this.lightProperties;
+};
+
+CS336Light.prototype.setLightProperties = function(lightProperties) {
+    this.lightProperties = lightProperties;
+};
+
+CS336Light.prototype.keyControl = function(ch) {
+    console.log(ch);
+
+    switch(ch) {
+        default: return false;
     }
 }

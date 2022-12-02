312d1f503ab7e8ac5f3883fe71beb253db2f6731
jsbento <benton_jacob@yahoo.com>
2022-12-01 20:19:05 -0600

    fix light

--- CS336Model.js
+++ CS336Model.js
@@ -49,6 +49,8 @@ CS336Model.prototype.loadModelBuffers = function() {
 CS336Model.prototype.render = async function(gl, worldMatrix, lights, camera) {
     const world = new THREE.Matrix4().copy(worldMatrix).multiply(this.getMatrix());
 
+    console.log(lights)
+
     if( this.draw ) await this.renderSelf(gl, world, lights, camera);
     
     for( let i = 0; i < this.children.length; i++ ) {
